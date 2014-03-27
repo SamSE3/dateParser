fbd5315453dbb6ee6b492c7e9f6a6fcd1d6f3223
SamSE3 <1.sam.d.9@gmail.com>
2014-03-27 15:16:04 +1030

    first attempt and shift left function, changed srandom to srand

--- tilt.c
+++ tilt.c
@@ -3,12 +3,16 @@
 
 int tilt_line_left(int length,int *line)
 {
+    int i;
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
-
-  // slide tiles to the left
-
-  // combine tiles as required
-
+        int valL = line[0];
+  for (i=0; i<(length-1); i++){
+      int valR= line[i+1];
+      if (valL==valR){
+          valL=2*valL;
+      }
+      valL=valR;
+  }
   return 0;
 }
