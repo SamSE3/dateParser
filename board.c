67ba37434abdbf3ae2b96571cc306e925a03e39a
SamSE3 <1.sam.d.9@gmail.com>
2014-04-03 15:06:09 +1030

    function to copy board and test board written ... lots of errors to fix!

--- board.c
+++ board.c
@@ -2,35 +2,45 @@
 #include <stdlib.h>
 #include "2048.h"
 
-int **board_create(int size)
-{
-  int i;
-  // make sure size of board is sensible
-  if (size<1||size>255) return NULL;
+int **board_create(int size) {
+    int i;
+    // make sure size of board is sensible
+    if (size < 1 || size > 255) return NULL;
 
-  int **board=calloc(size,sizeof(int *));
-  if (!board) return NULL;
-  for(i=0;i<size;i++) {
-    board[i]=calloc(size,sizeof(int));
-    if (!board[i]) return NULL;
-  }
-  return board;
-}
-
-int board_display(int size,int **board)
-{
-  // make sure size of board is sensible
-  if (size<1||size>255) return -1;
-  
-  int x,y;
-  
-  for(y=0;y<size;y++) {    
-    for(x=0;x<size;x++) {
-      if (board[x][y]) printf("[% 5d]",board[x][y]);
-      else printf("[     ]");
+    int **board = calloc(size, sizeof (int *));
+    if (!board) return NULL;
+    for (i = 0; i < size; i++) {
+        board[i] = calloc(size, sizeof (int));
+        if (!board[i]) return NULL;
     }
-    printf("\n");
-  }
-  return 0;
+    return board;
 }
 
+int board_display(int size, int **board) {
+    // make sure size of board is sensible
+    if (size < 1 || size > 255) return -1;
+
+    int x, y;
+
+    for (y = 0; y < size; y++) {
+        for (x = 0; x < size; x++) {
+            if (board[x][y]) printf("[% 5d]", board[x][y]);
+            else printf("[     ]");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+int ** boardCopy(int size, int **board) {
+    if (size < 1 || size > 255) return -1;
+    int **boardCopy = board_create(size);
+    memcpy(boardCopy, board, size * size);
+    /* int x,y;
+      for(y=0;y<size;y++) {    
+      for(x=0;x<size;x++) {
+          boardCopy[x][y]
+      }
+      }*/
+    return boardCopy;
+}
