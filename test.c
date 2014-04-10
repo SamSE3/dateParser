6a8a82dce91f7bc1a02f9f8f82e8c332295646c2
SamSE3 <1.sam.d.9@gmail.com>
2014-04-10 13:22:38 +0930

    test board left for a set board size of 4 now works - will write some more test for it shortly

--- test.c
+++ test.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+//#include "board.c"
 #include "2048.h"
 
 int line_vector_test(int i1, int i2, int i3, int i4, char *msg,
@@ -23,40 +25,43 @@ int line_vector_test(int i1, int i2, int i3, int i4, char *msg,
 
 }
 
-int board_vector_test(int **boardI, char *msg, int **boardO, int (*func)(int, int **)) {
-    int bad = 0;
+int board_vector_test(int **boardI, char *msg, int **boardO, int size, int (*func)(int, int **)) {
+    int x, y, bad = 0;
+    //printf("copying ...\n");
+    int **boardC = boardCopy(size, boardI);
     if (msg) printf("%s - ", msg);
     else {
-        printf("Tilting ");
-        board_display(4, int **board);
-        printf("left yields ");
-        board_display(4, int **board);
-
+        printf("Tilting\n");
+        board_display(size, boardI);
+        printf("left yields\n");
+        board_display(size, boardO);
     }
+
     fflush(stdout);
-    func(4, board);
+    func(size, boardI);
 
     for (y = 0; y < size; y++) {
         for (x = 0; x < size; x++) {
-            if (boardI[x][y] != boardI[x][y]) {
+            if (boardI[x][y] != boardC[x][y]) {
+                //printf("[% 5d]", boardI[x][y]);
                 x = size;
                 y = size;
                 bad = 1;
-                break;
             };
         }
     }
-
+printf("\n");
     if (bad == 1) {
-        printf("FAILED: {%d,%d,%d,%d} became {%d,%d,%d,%d} instead of"
-                " {%d,%d,%d,%d}\n",
-                i1, i2, i3, i4, list[0], list[1], list[2], list[3],
-                o1, o2, o3, o4);
+        printf("FAILED:\n");
+        board_display(size, boardC);
+        printf("became\n");
+        board_display(size, boardI);
+        printf("instead of\n");
+        board_display(size, boardO);
         return -1;
     }
     printf("PASSED.\n");
     return 0;
-
 }
 
 int ttl_vector(int i1, int i2, int i3, int i4, char *msg,
@@ -64,9 +69,8 @@ int ttl_vector(int i1, int i2, int i3, int i4, char *msg,
     return line_vector_test(i1, i2, i3, i4, msg, o1, o2, o3, o4, tilt_line_left);
 }
 
-int tt2_vector(int i1, int i2, int i3, int i4, char *msg,
-        int o1, int o2, int o3, int o4) {
-    return line_vector_test(i1, i2, i3, i4, msg, o1, o2, o3, o4, tilt_line_right);
+int tt2_vector(int **boardI, char *msg, int **boardO) {
+    return board_vector_test(boardI, msg, boardO, 4, tilt_board_left);
 }
 
 int test_tilt_left() {
@@ -78,6 +82,7 @@ int test_tilt_left() {
     e |= ttl_vector(1, 2, 4, 8, "Distinct values don't combine", 1, 2, 4, 8);
     e |= ttl_vector(1, 1, 1, 1, "Combinations don't cascade", 2, 2, 0, 0);
     e |= ttl_vector(2, 1, 1, 2, "Combinations don't cascade 2", 2, 2, 2, 0);
+    e |= ttl_vector(2, 0, 1, 1, "Combinations don't cascade 3", 2, 2, 0, 0);
     e |= ttl_vector(1, 0, 0, 2, "Bring together", 1, 2, 0, 0);
     e |= ttl_vector(0, 0, 1, 1, NULL, 2, 0, 0, 0);
     e |= ttl_vector(4, 0, 1, 1, NULL, 4, 2, 0, 0);
@@ -85,8 +90,35 @@ int test_tilt_left() {
     return e;
 }
 
+int test_board_left() {
+
+int **iBoard=alloca(4*sizeof(int*));
+//iBoard[0] = =alloca(sizeof(int)*4);
+//iBoard[1] = =alloca(sizeof(int)*4);
+//iBoard[2] = =alloca(sizeof(int)*4);
+//iBoard[3] = =alloca(sizeof(int)*4);
+
+int ib0[4]={0,0,0,0};
+int ib1[4]={0,0,0,0};
+int ib2[4]={0,0,0,0};
+int ib3[4]={0,0,0,0};
+
+iBoard[0]=&ib0[0];
+iBoard[1]=&ib1[0];
+iBoard[2]=&ib2[0];
+iBoard[3]=&ib3[0];
+
+int **oBoard = boardCopy(4,iBoard);
+
+    int e = 0;
+    e |= tt2_vector(board_create(4), NULL, board_create(4));
+    e |= tt2_vector(iBoard, NULL, oBoard);   
+    return e;
+}
+
 int main(int argc, char **argv) {
     int e = 0;
+    e |= test_board_left();
     e |= test_tilt_left();
     return e;
 }
