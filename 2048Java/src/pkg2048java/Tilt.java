d87013822519e4c001b83a24c2078049c6d72471
SuperNova <SuperNova@10.14.16.55>
2014-05-08 15:23:08 +0930

    added tilt value to the right, print row and reverse row methods

--- 2048Java/src/pkg2048java/Tilt.java
+++ 2048Java/src/pkg2048java/Tilt.java
@@ -36,15 +36,47 @@ public class Tilt {
         return row;
     }
 
-    public static int[] tiltRight(int[] row) {
-        return null;
+    public static int[] tiltRight(int[] row) {        
+        int lastValIndex = row.length;
+        boolean lastValCanCombine = true;
+        if (row[row.length - 1] != 0) {
+            lastValIndex = row.length - 1;
+        }
+        for (int i = lastValIndex - 1; i > -1; i--) { 
+            //start at the first non-zero value
+            if (lastValIndex < row.length && row[i] == row[lastValIndex] && lastValCanCombine) {
+                //double value
+                row[lastValIndex] *= 2;
+                row[i] = 0;
+                lastValCanCombine = false;
+            } else if (row[i] > 0) {
+                // move value to the right
+                lastValIndex--;
+                row[lastValIndex] = row[i];
+                if (i != lastValIndex) {
+                    row[i] = 0;
+                }
+                lastValCanCombine = true;
+            }
+        }
+        return row;        
     }
 
     public static void printRow(int[] row) {
+        String str="[";
+        for(int i=0; i<row.length-1; i++){
+            str+=row[i]+" ";
+        }
+        System.out.println(str+row[row.length-1]+"]");
     }
 
     public static int[] reverseArray(int[] row) {
-        return null;
+        for(int i=0; i<row.length/2; i++){
+            int temp = row[i];
+            row[i]=row[row.length-i-1];
+            row[row.length-i-1]=temp;
+        }
+        return row;
     }
 
 }
