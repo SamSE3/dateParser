ea5c8be714ea05b68e370da54954e66c77949bb2
SuperNova <SuperNova@10.14.16.55>
2014-05-08 15:43:02 +0930

    added tests for adding randoms

--- 2048Java/test/pkg2048java/BoardTest.java
+++ 2048Java/test/pkg2048java/BoardTest.java
@@ -40,25 +40,40 @@ public class BoardTest {
     @Test
     public void testAddRandom_intArrArr() {
         System.out.println("addRandom");
-        int[][] board = null;
-        boolean expResult = false;
-        boolean result = Board.addRandom(board);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals("does not say it inserts numbers when it can", true, Board.addRandom(board));
+        assertEquals("does not say it inserts numbers when it can", true, Board.addRandom(board0));
+        assertEquals("says it inserts numbers when it cant", false, Board.addRandom(board1));
+        int[][] board1Org = Board.makeCopy(board1);
+        Board.addRandom(board1);
+        assertArrayEquals("overwrites number in array", board1, board1Org);
     }
 
     @Test
     public void testAddRandom_intArrArr_int() {
         System.out.println("addRandom");
-        int[][] board = null;
-        int nosToAdd = 0;
-        boolean expResult = false;
-        boolean result = Board.addRandom(board, nosToAdd);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        int nosToAdd = 2;
+        int org, res;
+        assertEquals("sub method numbers does not work", 2, numbers(new int[][]{{1,0},{0,1}}));        
+        org = numbers(board);
+        Board.addRandom(board, nosToAdd);
+        res = numbers(board);
+        assertEquals("does not add the right amount of numbers", nosToAdd,res - org);
+        org=16;
+        assertEquals("sets a number to zero",  numbers(board1),16);
+        res = numbers(board1);
+    }    
+
+    private int numbers(int[][] board) {
+        int nums = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] != 0) {
+                    nums++;
+                }
+            }
+        }
+        return nums;
+    }    
 
     @Test
     public void testMakeRandomBoard() {
@@ -89,9 +104,9 @@ public class BoardTest {
         {2, 0, 0, 0},
         {4, 4, 4, 0},
         {0, 0, 0, 0}};
-        //Board.printBoardSimple(board2);
+        //Board.printBoard(board2);
         Board.tiltBoardLeft(board);
-        //Board.printBoardSimple(board);
+        //Board.printBoard(board);
         org.junit.Assert.assertArrayEquals("tilt board left", board2, board);
     }
 
@@ -102,57 +117,63 @@ public class BoardTest {
         {0, 0, 0, 2},
         {0, 4, 4, 4},
         {0, 0, 0, 0}};
-        //Board.printBoardSimple(board2);
+        //Board.printBoard(board2);
         Board.tiltBoardRight(board);
-        //Board.printBoardSimple(board);
+        //Board.printBoard(board);
         org.junit.Assert.assertArrayEquals("tilt board right", board2, board);
     }
     
-    @Test
-    public void testTiltBoardDown() {
-        System.out.println("tiltBoardDown");
-        int[][] board = null;
-        Board.tiltBoardDown(board);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
     @Test
     public void testTiltBoardUp() {
         System.out.println("tiltBoardUp");
-        int[][] board = null;
+        int[][] board2 = {{2, 4, 3, 4},
+        {4, 0, 2, 1},
+        {0, 0, 0, 4},
+        {0, 0, 0, 0}};
+        //Board.printBoard(board2);
         Board.tiltBoardUp(board);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Board.printBoard(board);
+        org.junit.Assert.assertArrayEquals("tilt board up", board2, board);
     }
+
+    @Test
+    public void testTiltBoardDown() {
+        System.out.println("tiltBoardDown");
+        int[][] board2 = {{0, 0, 0, 0},
+        {0, 0, 0, 4},
+        {2, 0, 3, 1},
+        {4, 4, 2, 4}};
+        //Board.printBoard(board2);
+        Board.tiltBoardDown(board);
+        //Board.printBoard(board);
+        org.junit.Assert.assertArrayEquals("tilt board down", board2, board);
+    }
+
 
     @Test
     public void testFlipBoardLeft() {
         System.out.println("flipBoardLeft");
-        int[][] board = null;
         Board.flipBoardLeft(board);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //Board.printBoard(board);
+        assertArrayEquals("arrays are not being flipped correctly", board, new int[][]{
+            {1, 1, 4, 0},
+            {2, 0, 2, 0},
+            {3, 0, 2, 0},
+            {4, 1, 4, 0}});
     }
 
     @Test
     public void testMakeCopy() {
         System.out.println("makeCopy");
-        int[][] board = null;
-        int[][] expResult = null;
-        int[][] result = Board.makeCopy(board);
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        int[][] copy = Board.makeCopy(board);
+        assertArrayEquals(board, copy);
     }
 
     @Test
     public void testPrintBoard() {
         System.out.println("printBoard");
-        int[][] board = null;
-        Board.printBoard(board);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Board.printBoard(board);        
     }
     
 }
