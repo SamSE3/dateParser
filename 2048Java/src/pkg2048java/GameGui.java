a913c5441ce239cadcd213e1cd90f3a3565db854
SuperNova <SuperNova@10.14.12.170>
2014-05-09 12:31:05 +0930

    added keypress event to make board move using board tilts

--- 2048Java/src/pkg2048java/GameGui.java
+++ 2048Java/src/pkg2048java/GameGui.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package pkg2048java;
 
 /**
@@ -13,7 +12,7 @@ package pkg2048java;
 public class GameGui extends javax.swing.JFrame {
 
     private int[][] board;
-    
+
     /**
      * Creates new form GameGui
      */
@@ -50,8 +49,62 @@ public class GameGui extends javax.swing.JFrame {
         jButtonStart = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+        });
+
+        jTF00.setEditable(false);
+        jTF00.setFocusable(false);
+
+        jTF20.setEditable(false);
+        jTF20.setFocusable(false);
+
+        jTF30.setEditable(false);
+        jTF30.setFocusable(false);
+
+        jTF10.setEditable(false);
+        jTF10.setFocusable(false);
+
+        jTF11.setEditable(false);
+        jTF11.setFocusable(false);
+
+        jTF01.setEditable(false);
+        jTF01.setFocusable(false);
+
+        jTF12.setEditable(false);
+        jTF12.setFocusable(false);
+
+        jTF13.setEditable(false);
+        jTF13.setFocusable(false);
+
+        jTF02.setEditable(false);
+        jTF02.setFocusable(false);
+
+        jTF33.setEditable(false);
+        jTF33.setFocusable(false);
+
+        jTF31.setEditable(false);
+        jTF31.setFocusable(false);
+
+        jTF23.setEditable(false);
+        jTF23.setFocusable(false);
+
+        jTF22.setEditable(false);
+        jTF22.setFocusable(false);
+
+        jTF21.setEditable(false);
+        jTF21.setFocusable(false);
+
+        jTF32.setEditable(false);
+        jTF32.setFocusable(false);
+
+        jTF03.setEditable(false);
+        jTF03.setFocusable(false);
 
         jButtonStart.setText("Start");
+        jButtonStart.setFocusable(false);
         jButtonStart.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonStartActionPerformed(evt);
@@ -145,34 +198,50 @@ public class GameGui extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonStartActionPerformed
-        board=Board.makeRandomBoard(4);
+        board = Board.makeRandomBoard(4);
         updateBoard();
     }//GEN-LAST:event_jButtonStartActionPerformed
 
-    private void updateBoard(){
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        System.out.println(evt.getKeyCode());
+        switch (evt.getKeyCode()) {
+            case 37:
+                Board.tiltBoardLeft(board);
+                break;
+            case 38:
+                Board.tiltBoardUp(board);
+                break;
+            case 39:
+                Board.tiltBoardRight(board);
+                break;
+            case 40:
+                Board.tiltBoardDown(board);
+        }
+        updateBoard();
+    }//GEN-LAST:event_formKeyPressed
+
+    private void updateBoard() {
         jTF00.setText(Integer.toString(board[0][0]));
         jTF10.setText(Integer.toString(board[1][0]));
         jTF20.setText(Integer.toString(board[2][0]));
         jTF30.setText(Integer.toString(board[3][0]));
-        
+
         jTF01.setText(Integer.toString(board[0][1]));
         jTF11.setText(Integer.toString(board[1][1]));
         jTF21.setText(Integer.toString(board[2][1]));
         jTF31.setText(Integer.toString(board[3][1]));
-        
+
         jTF02.setText(Integer.toString(board[0][2]));
         jTF12.setText(Integer.toString(board[1][2]));
         jTF22.setText(Integer.toString(board[2][2]));
         jTF32.setText(Integer.toString(board[3][2]));
-        
+
         jTF03.setText(Integer.toString(board[0][3]));
         jTF13.setText(Integer.toString(board[1][3]));
         jTF23.setText(Integer.toString(board[2][3]));
-        jTF33.setText(Integer.toString(board[3][3]));        
+        jTF33.setText(Integer.toString(board[3][3]));
     }
-    
-    
-    
+
     /**
      * @param args the command line arguments
      */
